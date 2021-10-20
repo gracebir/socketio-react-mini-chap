7ae26706ed1989dc2b5791d4dd380b7c47068e2a
graceDev1 <grace.devolop@gmail.com>
2021-10-20 20:32:11 +0200

    connect socket.id

--- server/main.js
+++ server/main.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const { Server } = require('socket.io')
 
 const app = express()
 
 app.use(cors())
 
 const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin:'http://localhost:3000',
+        methods: ["POST","GET"]
+    }
+})
+
+io.on("connection",(socket)=>{
+    console.log(socket.id);
+    socket.on("disconnect",()=>{
+        console.log("user disconnected")
+    })
+})
 
 server.listen(3001,()=>{
     console.log("server running")
